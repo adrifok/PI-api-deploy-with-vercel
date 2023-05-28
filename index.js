4da0b9b65196fe0baa00525681c8f7fe36b7c9b5
adrifok <adrifok@gmail.com>
2023-05-28 14:59:20 -0500

    restructuring code

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const  {PORT} = process.env;
-const port = PORT || 3001;
-
+//const port = PORT || 3001;
+require('dotenv').config();
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(port, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log('%s listening at' , process.env.PORT); // eslint-disable-line no-console
   });
 });
